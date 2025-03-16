075567b5c6280cc31e5428ee34b6aa82783d3bdd
bombaclath97 <e.bombardelli97@gmail.com>
2025-03-16 14:49:49 +0100

    Remove Membership from user model

--- mensa-shared-models/userModel.go
+++ mensa-shared-models/userModel.go
@@ -1,10 +1,9 @@
 package model
 
 type User struct {
-	TelegramID        int64   `json:"telegramId"`
-	MensaEmail        string  `json:"mensaEmail"`
-	MembershipEndDate *string `json:"membershipEndDate,omitempty"`
-	FirstName         string  `json:"firstName"`
-	LastName          string  `json:"lastName"`
-	MemberNumber      int64   `json:"memberNumber"`
+	TelegramID   int64  `json:"telegramId"`
+	MensaEmail   string `json:"mensaEmail"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	MemberNumber int64  `json:"memberNumber"`
 }
