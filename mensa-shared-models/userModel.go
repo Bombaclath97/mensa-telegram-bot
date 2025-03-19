838d2d8978cd8b5789d16c24bbcb577ba4a42c0e
Bombaclath97 <e.bombardelli97@gmail.com>
2025-03-19 14:48:55 +0100

    Add IsInternational field

--- mensa-shared-models/userModel.go
+++ mensa-shared-models/userModel.go
@@ -1,10 +1,11 @@
 package model
 
 type User struct {
-	TelegramID   int64  `json:"telegramId"`
-	MensaEmail   string `json:"mensaEmail"`
-	FirstName    string `json:"firstName"`
-	LastName     string `json:"lastName"`
-	MemberNumber int64  `json:"memberNumber"`
-	IsBotAdmin   bool   `json:"isBotAdmin"`
+	TelegramID      int64  `json:"telegramId"`
+	MensaEmail      string `json:"mensaEmail"`
+	FirstName       string `json:"firstName"`
+	LastName        string `json:"lastName"`
+	MemberNumber    int64  `json:"memberNumber"`
+	IsBotAdmin      bool   `json:"isBotAdmin"`
+	IsInternational bool   `json:"isInternational"`
 }
